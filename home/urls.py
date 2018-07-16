33e5324b3294318d4039977a03efb2eabf7f7a4c
jayeshsingh9767 <jayeshsingh9767@gmail.com>
2018-07-16 15:07:14 +0530

    Added Details url and Linked it with Photo id

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),                                 # home
+    url(r'^(?P<photo_id>[0-9]+)/$', views.details, name='details')     # home/<photo_id>
 ]
 
--- home/views.py
+++ home/views.py
@@ -2,43 +2,52 @@ from django.shortcuts import render,HttpResponse
 from .models import *
 from django.template import loader
 # Create your views here.
-cat = Categories.objects.all()
 
 
 def cat_name(cname):
     return cat.get(category_name=cname)
 
 
-def home(request):
-    all_photos = Photo.objects.all()
-    # cat = Categories.objects.all()
-    auther = Photographer.objects.all()
-    city_pic = Photo.objects.filter(category=cat.get(category_name='City'))
-    nature_pic = Photo.objects.filter(category=cat.get(category_name='Nature'))
-    brand_pic = Photo.objects.filter(category=cat.get(category_name='Brand Logo'))[:5]
-    devotion_pic = Photo.objects.filter(category=cat.get(category_name='Devotional'))
-    back_pic = Photo.objects.filter(category=cat.get(category_name='Background'))
-    mountain_pic = Photo.objects.filter(category=cat.get(category_name='Mountains'))
-    animal_pic = Photo.objects.filter(category=cat.get(category_name='Animals'))
+all_photos = Photo.objects.all()
+cat = Categories.objects.all()
+auther = Photographer.objects.all()
+city_pic = Photo.objects.filter(category=cat.get(category_name='City'))
+nature_pic = Photo.objects.filter(category=cat.get(category_name='Nature'))
+brand_pic = Photo.objects.filter(category=cat.get(category_name='Brand Logo'))[:5]
+devotion_pic = Photo.objects.filter(category=cat.get(category_name='Devotional'))
+back_pic = Photo.objects.filter(category=cat.get(category_name='Background'))
+mountain_pic = Photo.objects.filter(category=cat.get(category_name='Mountains'))
+animal_pic = Photo.objects.filter(category=cat.get(category_name='Animals'))
 
+context = {
+    'all_photos': all_photos,
+    'cat': cat,
+    'auther': auther,
+    'city_pic': city_pic,
+    'brand_pic': brand_pic,
+    'nature_pic': nature_pic,
+    'devotion_pic': devotion_pic,
+    'back_pic': back_pic,
+    'mountain_pic': mountain_pic,
+    'animal_pic': animal_pic,
+    'cat_name_city': cat_name('City'),
+    'cat_name_nature': cat_name('Nature'),
+    'cat_name_animal': cat_name('Animals'),
+    'cat_name_mountain': cat_name('Mountains'),
+    'cat_name_brand': cat_name('Brand Logo'),
+    'cat_name_devotion': cat_name('Devotional'),
+}
+
+
+def home(request):
     template = loader.get_template('home.html')
-    context = {
-        'all_photos': all_photos,
-        'cat': cat,
-        'auther': auther,
-        'city_pic': city_pic,
-        'brand_pic': brand_pic,
-        'nature_pic': nature_pic,
-        'devotion_pic': devotion_pic,
-        'back_pic': back_pic,
-        'mountain_pic': mountain_pic,
-        'animal_pic': animal_pic,
-        'cat_name_city': cat_name('City'),
-        'cat_name_nature': cat_name('Nature'),
-        'cat_name_animal': cat_name('Animals'),
-        'cat_name_mountain': cat_name('Mountains'),
-        'cat_name_brand': cat_name('Brand Logo'),
-        'cat_name_devotion': cat_name('Devotional'),
-    }
 
     return HttpResponse(template.render(context, request))
+
+
+def details(request, photo_id):
+    template = loader.get_template('details.html')
+    context['photo_id'] = photo_id
+    return HttpResponse(template.render(context, request))
+
+
